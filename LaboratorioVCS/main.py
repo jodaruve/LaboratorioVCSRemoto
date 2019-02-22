92ebb972a6afb8b9a34346740601d04b7c52335a
jodaruve <jose.rueda.2019@ubp.edu.co>
2019-02-22 11:00:23 -0500

    mi primer commit

---
+++ LaboratorioVCS/main.py
@@ -0,0 +1,7 @@
+a=int(input("Por favor ingrese un valor "))
+c=int(input("Por favor ingrece otro valor "))
+producto=a*c
+d_a=a*2
+print("el producto de", a, "y", c, "es:", producto)
+print("el doble del número", a, "es:", d_a)
+
